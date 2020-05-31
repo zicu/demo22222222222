d8a69508d1f8ee54c3b113353bfc16109a89d39f
zicu <3461780800@qq.com>
2020-05-31 15:32:05 +0800

    oko

---
+++ src/main/java/com/example/demo22222222222/Demo22222222222Application.java
@@ -0,0 +1,13 @@
+package com.example.demo22222222222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo22222222222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo22222222222Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo22222222222/Demo22222222222ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo22222222222;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo22222222222ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
